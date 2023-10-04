63161ca481a8a3a411e8609ad3ce4f6e1cd941d6
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2023-10-04 18:54:34 -0300

    implemented transition between scenes

---
+++ Assets/Scripts/Utils/LoadScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Load(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void UnloadAll()
+    {
+        Application.Quit();
+    }
+}
