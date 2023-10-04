c4d2e77c2a42d1e34f0d6165fd4eee22801733d7
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2023-10-04 19:56:21 -0300

    fixed organization and clarity issues

--- Assets/Scripts/Utils/LoadScene.cs
+++ Assets/Scripts/Utils/LoadScene.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// O script implementa transições entre as cenas,
+// bem como a opção de sair do jogo.
+
 public class LoadScene : MonoBehaviour
 {
     public void Load(int sceneIndex)
